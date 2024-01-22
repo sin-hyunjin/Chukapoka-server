aea1d31b3d5741ec45c4a8c7a0d1b5ee0c485680
doyou1 <tla149@daum.net>
2024-01-22 15:37:15 +0900

    domain url http & https

--- src/main/java/com/chukapoka/server/WebConfig.java
+++ src/main/java/com/chukapoka/server/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080", _env.getProperty("DOMAIN_URL"))
+                .allowedOrigins("http://localhost:8080", _env.getProperty("DOMAIN_URL_HTTP"), _env.getProperty("DOMAIN_URL_HTTPS"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
