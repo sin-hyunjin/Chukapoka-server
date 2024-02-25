45c15c356d3f36ba78da0ec82a84bdcbfeb7ad9d
Hyun jin <blackduvet52@gmail.com>
2024-02-25 12:37:26 +0900

    - Tree상세정보 메서드 추가    - TreeDetailResponseDto 추가 (treeitem 필드 필요) - 상세정보를 위한 modelMapper 추가

--- src/main/java/com/chukapoka/server/tree/service/TreeServiceImpl.java
+++ src/main/java/com/chukapoka/server/tree/service/TreeServiceImpl.java
@@ -1,10 +1,7 @@
 package com.chukapoka.server.tree.service;
 
 import com.chukapoka.server.common.dto.CustomUser;
-import com.chukapoka.server.tree.dto.TreeList;
-import com.chukapoka.server.tree.dto.TreeListResponseDto;
-import com.chukapoka.server.tree.dto.TreeRequestDto;
-import com.chukapoka.server.tree.dto.TreeModifyRequestDto;
+import com.chukapoka.server.tree.dto.*;
 import com.chukapoka.server.tree.entity.Tree;
 import com.chukapoka.server.tree.repository.TreeRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -15,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,11 +24,14 @@ public class TreeServiceImpl implements TreeService{
     /** 트리생성 */
     @Override
     @Transactional
-    public Tree createTree(TreeRequestDto treeRequestDto) {
+    public Tree createTree(TreeCreateRequestDto treeRequestDto) {
         Tree tree = new Tree();
         // 클라이언트에서 입력 받을 필요없이 토큰으로 접속후 권한id로 셋팅
         long userId = ((CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId();
         treeRequestDto.setUpdatedBy(userId);
+
+        // 질문 : 여기를 build를 사용하는게 좋을지 modelMapper를 사용하는게 좋을지??
+        // -> 서버에서 linkId, sendId를 만들어줄꺼라면 build가 좋을꺼같은데...
         BeanUtils.copyProperties(treeRequestDto, tree); // mapper대신 사용할수 있지만 복사할 속성의 수가 적고 속성 이름이 일치하는 경우에 적합
         return treeRepository.save(tree);
     }
@@ -41,43 +40,38 @@ public class TreeServiceImpl implements TreeService{
     @Override
     public TreeListResponseDto treeList() {
         List<TreeList> trees = treeRepository.findAllTrees();
-        if (trees.isEmpty()) {
-            return new TreeListResponseDto(new ArrayList<>());
-        }
         return new TreeListResponseDto(trees);
     }
 
     /** 트리 상세 정보 조회 (상세정보 모델) */
     @Override
-    public Tree treeDetail(Long treeId) {
-
-        return null;
+    public TreeDetailResponseDto treeDetail(Long treeId) {
+        Tree tree = findTreeByIdOrThrow(treeId);
+        // tree entity를 TreeDetailResponseDto로 매핑
+        return modelMapper.map(tree, TreeDetailResponseDto.class);
     }
 
     /** 트리수정 */
     @Override
-    @Transactional // 데이터 변경감지
     public Tree treeModify(Long treeId, TreeModifyRequestDto treeModifyDto) {
         // 트리 아이디로 트리를 찾음
-        Tree tree = treeRepository.findById(treeId)
-                .orElseThrow(() -> new EntityNotFoundException("treeId " + treeId + " 를 찾을 수 없습니다."));
-        updateTreeAttributes(tree, treeModifyDto);
-        return treeRepository.save(tree);
-
+        Tree tree = findTreeByIdOrThrow(treeId);
+        return treeUpdate(tree, treeModifyDto);
     }
 
     /** 트리 삭제 */
     @Override
     @Transactional
-    public void deleteTree(Long treeId) {
-        treeRepository.findById(treeId)
-                .orElseThrow(() -> new EntityNotFoundException("등록되지 않은 " + treeId + "입니다."));
+    public void treeDelete(Long treeId) {
+        findTreeByIdOrThrow(treeId);
         treeRepository.deleteById(treeId);
     }
 
 
-    /** 트리필드 정보 업데이트 */
-    private void updateTreeAttributes(Tree tree, TreeModifyRequestDto treeModifyDto) {
+
+    /** 트리필드 정보 업데이트 메서드*/
+    @Transactional // 데이터 변경감지
+    private Tree treeUpdate(Tree tree, TreeModifyRequestDto treeModifyDto) {
         tree.setTitle(treeModifyDto.getTitle());
         tree.setType(treeModifyDto.getType());
         tree.setTreeBgColor(treeModifyDto.getTreeBgColor());
@@ -85,5 +79,12 @@ public class TreeServiceImpl implements TreeService{
         tree.setTreeTopColor(treeModifyDto.getTreeTopColor());
         tree.setTreeItemColor(treeModifyDto.getTreeItemColor());
         tree.setTreeBottomColor(treeModifyDto.getTreeBottomColor());
+        return treeRepository.save(tree);
+    }
+
+    /** treeId Exception 처리 메서드 */
+    private Tree findTreeByIdOrThrow(Long treeId) {
+        return treeRepository.findById(treeId)
+                .orElseThrow(() -> new EntityNotFoundException("등록되지 않은 " + treeId + "입니다."));
     }
 }
